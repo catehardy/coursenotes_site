7ffe62b0a2eb1678f1ea8b157b1ba34ac1d49a49
Catherine Hardy <83373032+bonbonsai@users.noreply.github.com>
2022-02-07 22:29:03 +0000

    Change responsive topnav function to tidier code

--- topnav.js
+++ topnav.js
@@ -1,9 +1,14 @@
 // Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.classList.add("responsive");
+//   } else {
+//     x.classList.remove("responsive");
+//   }
+// } 
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.classList.add("responsive");
-  } else {
-    x.classList.remove("responsive");
-  }
-} 
+  x.classList.toggle("responsive");
+}
